bf9b6fcd68b41295bd4bc3d191705d583955c1ef
Sasindu Jayasinghe <sasindu.jayasinghe@gmail.com>
2020-10-17 11:05:25 +1300

    Update macOS Setup.sh

--- scripts/macOS Setup.sh
+++ scripts/macOS Setup.sh
@@ -127,6 +127,7 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
 
 defaults write com.knollsoft.Rectangle gapSize -float 10
 defaults write com.apple.Dock showhidden -bool TRUE; killall Dock
+defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
 
 # Remove garbage
 brew cleanup
