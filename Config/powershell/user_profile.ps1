6942d6afaddac1bae7a9c0ebb747c7edd5ac7f14
Sasindu Jayasinghe <sasindu.jayasinghe@gmail.com>
2023-07-09 15:10:31 +1000

    Update powershell config

--- Config/powershell/user_profile.ps1
+++ Config/powershell/user_profile.ps1
@@ -1,7 +1,8 @@
-# Alias
+# Aliases
 Set-Alias vim nvim
 Set-Alias open ii
-Set-Alias lg lazygit
+Set-Alias tig "C:\Users\sasin\scoop\apps\git\2.41.0.2\usr\bin\tig.exe"
+Set-Alias less "C:\Users\sasin\scoop\apps\git\2.41.0.2\usr\bin\less.exe"
 
-# Starship
+# Starship Prompt
 Invoke-Expression (&starship init powershell)
