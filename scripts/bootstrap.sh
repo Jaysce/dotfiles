646fd77333cf75482f34ed4a00b8b1bed3643a0b
Sasindu Jayasinghe <sasindu.jayasinghe@gmail.com>
2024-05-11 12:11:39 +1000

    Update bootstrap.sh

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd
+
 # update and upgrade
 sudo apt-get update -y
 sudo apt-get upgrade -y
@@ -20,10 +22,20 @@ sudo apt-get install -y gcc g++ make
 # cleanup
 sudo apt autoremove -y
 
+cd
+
 # dotfiles
 git clone https://github.com/Jaysce/dotfiles.git
 mkdir -p ~/.config
 ln -sv ~/dotfiles/Config/.gitconfig ~/.gitconfig
 ln -sv ~/dotfiles/Config/tmux/.tmux.conf ~/.tmux.conf
-ln -sv ~/dotfiles/Config/.zshrc ~/.zshrc
 ln -sv ~/dotfiles/Config/starship.toml ~/.config
+
+# prompt / omz
+rm .zshrc
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+curl -sS https://starship.rs/install.sh | sh
+ln -sv ~/dotfiles/Config/.zshrc ~/.zshrc
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
