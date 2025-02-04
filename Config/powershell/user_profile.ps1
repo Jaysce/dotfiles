1b8a307a2314296762254557b2d9d9e4483493f9
Sasindu Jayasinghe <sasindu.jayasinghe@gmail.com>
2025-02-05 10:42:40 +1100

    update user_profile

--- Config/powershell/user_profile.ps1
+++ Config/powershell/user_profile.ps1
@@ -1,8 +1,7 @@
 # Aliases
 Set-Alias vim nvim
 Set-Alias open ii
-Set-Alias tig "C:\Users\sasin\scoop\apps\git\2.41.0.2\usr\bin\tig.exe"
-Set-Alias less "C:\Users\sasin\scoop\apps\git\2.41.0.2\usr\bin\less.exe"
+Set-Alias cat bat
 
 # Starship Prompt
 Invoke-Expression (&starship init powershell)
