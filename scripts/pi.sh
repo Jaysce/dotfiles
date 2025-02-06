89989f97a52c3e82fd7dadcc562d3f14369df38a
Sasindu Jayasinghe <sasindu.jayasinghe@gmail.com>
2025-02-06 23:54:35 +1100

    update pi.sh

--- scripts/pi.sh
+++ scripts/pi.sh
@@ -8,21 +8,13 @@ cd
 sudo apt update -y
 sudo apt upgrade -y
 
-# essentials
-sudo apt install -y curl wget git xclip bat unzip zip tmux zsh gh neovim
+# tools
+sudo apt install -y curl wget git xclip bat unzip zip tmux zsh gh
 
-# install 1password cli
-curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
-sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg && \
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/$(dpkg --print-architecture) stable main" | \
-sudo tee /etc/apt/sources.list.d/1password.list && \
-sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/ && \
-curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | \
-sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol && \
-sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22 && \
-curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
-sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg && \
-sudo apt install 1password-cli
+# git delta
+wget https://github.com/dandavison/delta/releases/download/0.18.2/git-delta_0.18.2_arm64.deb
+dpkg -i git-delta_0.18.2_arm64.deb
+rm git-delta_0.18.2_arm64.deb
 
 # cleanup
 sudo apt autoremove -y
