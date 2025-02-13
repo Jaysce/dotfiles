09a2b8e574871086b3387d4066f499bad6685b9c
Sasindu Jayasinghe <sasindu.jayasinghe@gmail.com>
2025-02-13 21:19:53 +1100

    Add arch bootstrap

---
+++ scripts/arch_bootstrap.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+# Ensure we are in the home directory
+cd ~
+
+# Update package manager
+sudo pacman -Syu
+
+# Install yay
+sudo pacman -S --needed git base-devel
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ..
+rm -rf yay
+
+# Install packages
+packages=(
+    bat
+    curl
+    github-cli
+    git-delta
+    neovim
+    starship
+    stow
+    tmux
+    wget
+    xclip
+    zsh
+    hyprland
+    dolphin
+    wofi
+    alacritty
+)
+
+for package in "${packages[@]}"; do
+    if ! pacman -Qi $package &> /dev/null; then
+        yay -S --noconfirm $package
+    else
+        echo "$package is already installed"
+    fi
+done
+
+# Install Oh My Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+if [ -f ~/.zshrc ]; then
+    rm ~/.zshrc
+fi
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Dotfiles
+mkdir -p ~/.config/starship
+mkdir -p ~/.ssh
+git clone https://github.com/Jaysce/dotfiles.git
+cd dotfiles
+stow .
+cd ~
+
+# Install fonts
+sudo yay -S $(yay -Ssq noto-fonts)
+yay -S --noconfirm ttf-firacode-nerd
+yay -S --noconfirm ttf-jetbrains-mono-nerd
+
+# Install TPM (Tmux Plugin Manager)
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+chsh -s $(which zsh)
