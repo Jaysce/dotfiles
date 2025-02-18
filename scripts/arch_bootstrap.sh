a42e2444bc0f4decbc5f2d7d9d08b5a32cdd4584
Sasindu Jayasinghe <sasindu.jayasinghe@gmail.com>
2025-02-19 01:31:11 +1100

    update arch_bootstrap

--- scripts/arch_bootstrap.sh
+++ scripts/arch_bootstrap.sh
@@ -19,12 +19,15 @@ rm -rf yay
 
 # Install packages
 packages=(
+    alacritty
+    amd-ucode
     bat
     bluez
     bluez-utils
     curl
     github-cli
     git-delta
+    firefox
     neovim
     starship
     stow
@@ -34,8 +37,14 @@ packages=(
     zsh
     hyprland
     dolphin
+    waybar
     wofi
-    alacritty
+    pamixer
+    refind
+    sbctl
+    ly
+    hyprpaper
+    openssh
 )
 
 for package in "${packages[@]}"; do
@@ -56,6 +65,7 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 
 # Dotfiles
 mkdir -p ~/.config/starship
+mkdir -p ~/.config/waybar
 mkdir -p ~/.ssh
 git clone https://github.com/Jaysce/dotfiles.git
 cd dotfiles
@@ -63,7 +73,7 @@ stow .
 cd ~
 
 # Install fonts
-sudo yay -S $(yay -Ssq noto-fonts)
+sudo pacman -S $(pacman -Ssq noto-fonts)
 yay -S --noconfirm ttf-firacode-nerd
 yay -S --noconfirm ttf-jetbrains-mono-nerd
 
@@ -73,5 +83,29 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 # Enable bluetooh
 systemctl enable bluetooth.service
 systemctl start bluetooth.service
+
+# Install refind
+refind-install
+# sudo mount --mkdir /dev/<your_device> /mnt/boot
+# cd /mnt/boot/EFI/refind
+# sudo mkdir themes
+# sudo git clone https://github.com/AdityaGarg8/rEFInd-minimal-modded.git ./themes
+# sudo mv ./themes/rEFInd-minimal-modded ./themes/rEFInd-minimal
+# sudo sh -c "echo \"include themes/rEFInd-minimal/theme.conf\" >> refind.conf"
+# sudo sh -c "echo \"resolution max\" >> refind.conf"
+
+## Setup secure boot ensure that secure boot is in setup mode by deleting all keys
+# sudo sbctl create-keys
+# sudo sbctl enroll-keys -m
+## Sign the bootloader and kernel
+# sudo sbctl sign -s -o /usr/lib/systemd/boot/efi/systemd-bootx64.efi.signed /usr/lib/systemd/boot/efi/systemd-bootx64.efi
+# sudo sbctl sign -s /boot/EFI/refind/refind_x64.efi
+# sudo sbctl sign -s /boot/vmlinuz-linux
+## Verify everything is signed
+# sudo sbctl verify
+
+# Enable login manager
+sudo systemctl enable ly.service
+sudo systemctl start ly.service
 
 chsh -s $(which zsh)
