0d50c8f788f3ff5cdd686823ba13dd0ee486b7d1
Sasindu Jayasinghe <sasindu.jayasinghe@gmail.com>
2025-06-08 00:46:44 +1000

    Update bootstrap.sh

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -9,7 +9,13 @@ sudo apt update -y
 sudo apt upgrade -y
 
 # essentials
-sudo apt install -y curl wget git xclip neovim bat unzip zip tmux zsh gh stow jq httpie
+sudo apt install -y curl wget git xclip neovim bat unzip zip tmux zsh gh stow jq httpie ripgrep
+
+# lazygit (taken from https://github.com/jesseduffield/lazygit?tab=readme-ov-file#debian-and-ubuntu)
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit -D -t /usr/local/bin/
 
 # cleanup
 sudo apt autoremove -y
